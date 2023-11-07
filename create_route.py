bad5648520b8a637f16f56a15d81f818edfe801e
PabloPatin <pashka.lop@mail.ru>
2023-11-07 23:31:47 +0300

    yandex API added

---
+++ create_route.py
@@ -0,0 +1,6 @@
+from yandex_API import ask_yandex_for_time_matrix
+
+address_list = ["Краснодар, В.Н. Мачуги, 41", "Краснодар, ул. Красная, д. 154",
+                    "Краснодар, ул. Красных Партизан, д. 117"]
+ask_yandex_for_time_matrix(*address_list)
+
